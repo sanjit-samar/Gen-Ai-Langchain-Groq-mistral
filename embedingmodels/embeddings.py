9cedfd1ee5d9830cd8b647f4bdc9e22c1cac4fae
sanjit samar <sanjitsamar07@gmail.com>
2026-04-19 17:47:31 +0530

    embedding mdel changes

--- embedingmodels/embeddings.py
+++ embedingmodels/embeddings.py
@@ -7,6 +7,14 @@ load_dotenv()
 embeddings = OpenAIEmbeddings(model="text-embedding-3-large", dimensions=64)
 # embeddings = MistralAIEmbeddings(model="mistral-embed", dimensions=64)
 
-vector = embeddings.embed_query("you are great")
+# vector = embeddings.embed_query("you are great")
+
+texts = [
+    "Where is the Silicon valley in USA",
+    "How many employees are working with microsoft",
+    "where is the capital of UK",
+]
+
+vector = embeddings.embed_documents(texts)
 
 print(vector)
